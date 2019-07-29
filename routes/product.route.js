30f27ccf657fb345eef5154ca213367366618e9b
hiteshproy@gmail.com <hiteshproy@gmail.com>
2019-07-29 23:39:34 +0530

    added basic backend code

---
+++ routes/product.route.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+
+const product_controller = require('../controllers/product.controller');
+
+router.get('/test', product_controller.test);
+// router.post('/create', product_controller.product_create);
+// router.get('/products', product_controller.product_details);
+// router.put('/:id/update', product_controller.product_update);
+
+
+module.exports = router;
