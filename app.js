04ae7dbf250c11cc09827935aab7e5dcd3b8d6b4
hiteshproy@gmail.com <hiteshproy@gmail.com>
2019-07-30 22:00:40 +0530

    added config file and database connection

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const product = require('./routes/product.route'); 
+const config = require('./config');
 const app = express();
 
 
 
-// // Set up mongoose connection
-// const mongoose = require('mongoose');
-// let dev_db_url = 'mongodb://hiteshpr:hitesh12345@ds111562.mlab.com:11562/products';
-// let mongoDB = process.env.MONGODB_URI || dev_db_url;
-// mongoose.connect(mongoDB);
-// mongoose.Promise = global.Promise;
-// let db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// Set up mongoose connection
+const mongoose = require('mongoose');
+const dev_db_url = 'mongodb://' + config.dbUsername + ':' + config.dbPassword + '@ds151007.mlab.com:51007/comment_app';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/api', product);
 
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || config.port;
 
 app.listen(PORT, () => {
-    console.log('Server is up and running on port ${ PORT }');
+    console.log('Server is up and running on port', PORT);
 });
---
+++ config.js
@@ -0,0 +1,10 @@
+
+
+var config = {};
+
+config.dbUsername = "hiteshdb";
+config.dbPassword = "hitesh12";
+
+config.port = 3000;
+
+module.exports = config;
